18f21ca1a04aeb7288c4da0b407b0f6ff302eae2
Daniel <tacombel@gmail.com>
2023-07-10 19:41:10 +0200

    Bug

--- 144blocks.py
+++ 144blocks.py
@@ -1,7 +1,8 @@
 from urllib.request import Request, urlopen
 import json
 
-url = 'https://explorer.scpri.me/navigator-api/hash/17F62043BF2B3A777C96110FA28C77FC0C0A183334F642D1300993EF6709008938F2E3918BE6'
+current_address = '502E8B71CFBDF5A92CF4DAACE154F83721BA016304339BEA6A9A9FDC5A6CEB0534CDA1B78E8F'
+url = 'https://explorer.scpri.me/navigator-api/hash/' + current_address
 req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
 response = urlopen(req)
 data = response.read()
@@ -12,6 +13,14 @@ transactions = []
 for e in data[1]['last100Transactions']:
     transactions.append(e)
 transactions.reverse()
+
+url = 'https://explorer.scpri.me/navigator-api/status'
+req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+response = urlopen(req)
+data = response.read()
+data = data.decode("utf-8")
+data = json.loads(data)
+current_block = data[0]['lastblock']
 
 first_bid = True
 current_bid = 0
@@ -40,16 +49,14 @@ for e in transactions:
             else:
                 print(f'Block:{e["Height"]}, bid: {e["ScChange"] / 1e27} SCP --> Bid must be between {(current_bid+ 1e27 / 1e27)} and {(current_bid + 100e27) / 1e27} SCP. Current winning bid: {current_bid / 1e27}scp')
 
-url = 'https://explorer.scpri.me/navigator-api/status'
-req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-response = urlopen(req)
-data = response.read()
-data = data.decode("utf-8")
-data = json.loads(data)
-current_block = data[0]['lastblock']
+if current_block > last_valid_bid_block + 144:
+    competion_on = False
 
 if competion_on:
     print()
-    print(f'Last valid block will be {last_valid_bid_block + 144}. There are {last_valid_bid_block + 144 - current_block} blocks to come before the end of the competition')
+    print(f'Last valid block will be {last_valid_bid_block + 144}.')
     print(f'There are still {last_valid_bid_block + 144 - current_block} blocks to come before the end of the competition. Aproximately {(last_valid_bid_block + 144 - current_block) / 6} hours')
     print(f'Valid bids between {(current_bid + 1e27) / 1e27} and {(current_bid + 100e27) / 1e27} scp, both included')
+else:
+    print()
+    print(f'The competition ended')
